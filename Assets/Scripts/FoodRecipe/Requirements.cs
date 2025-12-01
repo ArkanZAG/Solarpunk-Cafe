f65de50ec58bd9a65ea8c2b18abc16b6a924cea1
ArkanZAG <142731011+ArkanZAG@users.noreply.github.com>
2025-12-01 20:37:46 +0700

    Simplify

--- Assets/Scripts/FoodRecipe/Requirements.cs
+++ Assets/Scripts/FoodRecipe/Requirements.cs
@@ -1,23 +1,20 @@
+using UnityEngine.Serialization;
+
 namespace FoodRecipe
 {
     [System.Serializable]
     public class Requirements
     {
-        public RequirementSpecificType requirementSpecificType;
-        public RequireGeneralType requirementGeneralType;
+        public InggredientRequire inggredientRequire;
         public int quantity = 0;
         
-        public enum RequirementSpecificType
+        public enum InggredientRequire
         {
             None,
             Banana,
             Cassava,
             Clams,
-        }
-
-        public enum RequireGeneralType
-        {
-            None,
+            PalmSugar,
             AnyProtein,
             AnyFat,
             AnySugar,
